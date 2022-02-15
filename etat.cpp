86a92418b93f367328f81a671942baba758fedb8
Taha Mdarhri <t.mdarhri@gmail.com>
2022-02-15 11:20:23 +0100

    Etats

--- etat.cpp
+++ etat.cpp
@@ -1,13 +1,206 @@
 #include "etat.h"
 #include <iostream>
 
-Etat * Etat0::transition(Automate & d, Symbole s){
-    if(s == INT){
-        return new Etat3();
-    }else if(s == OPENPAR){
+bool *Etat0::transition(Automate &d, Symbole s)
+{
+    if (s == INT)
+    {
+        cout << "INT" << endl;
+        d.decalage(s, new Etat3());
+    }
+    else if (s == OPENPAR)
+    {
+        cout << "OPENPAR" << endl;
+        d.decalage(s, new Etat2());
+    }
+    else if (s == EXPR)
+    {
+        cout << "EXPR" << endl;
+        d.decalage(s, new Etat1());
+    }
+    return false;
+}
+
+Etat *Etat1::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        d.decalage(s, new Etat4());
+    }
+    else if (s == MULT)
+    {
+        d.decalage(s, new Etat5());
+    }
+    else if (s == FIN)
+    {
+        return true;
+    }
+    return false;
+}
+
+Etat *Etat2::transition(Automate &d, Symbole s)
+{
+    if (s == INT)
+    {
+        d.decalage(s, new Etat3());
+    }
+    else if (s == OPENPAR)
+    {
+        d.decalage(s, new Etat2());
+    }
+    else if (s == EXPR)
+    {
+        d.decalage(s, new Etat6());
+    }
+    return false;
+}
+
+Etat *Etat3::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        // regle 5
+        return new Etat0();
+    }
+    else if (s == MULT)
+    {
+        // regle 5
         return new Etat2();
-    }else if(s == EXPR){
+    }
+    else if (s == CLOSEPAR)
+    {
+        // regle 5
         return new Etat1();
     }
-    return nullptr;
+    else if (s == FIN)
+    {
+        // regle 5
+        return new Etat0();
+    }
+    return false;
+}
+
+Etat *Etat4::transition(Automate &d, Symbole s)
+{
+    if (s == INT)
+    {
+        d.decalage(s, new Etat3());
+    }
+    else if (s == OPENPAR)
+    {
+        d.decalage(s, new Etat2());
+    }
+    else if (s == EXPR)
+    {
+        d.decalage(s, new Etat7());
+    }
+    return false;
+}
+
+Etat *Etat5::transition(Automate &d, Symbole s)
+{
+    if (s == INT)
+    {
+        d.decalage(s, new Etat3());
+    }
+    else if (s == OPENPAR)
+    {
+        d.decalage(s, new Etat2());
+    }
+    else if (s == EXPR)
+    {
+        d.decalage(s, new Etat8());
+    }
+    return false;
+}
+
+Etat *Etat6::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        d.decalage(s, new Etat4());
+    }
+    else if (s == MULT)
+    {
+        d.decalage(s, new Etat5());
+    }
+    else if (s == CLOSEPAR)
+    {
+        d.decalage(s, new Etat9());
+    }
+    return false;
+}
+
+Etat *Etat7::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        // regle 2
+        return new Etat0();
+    }
+    else if (s == MULT)
+    {
+        d.decalage(s, new Etat5());
+    }
+    else if (s == CLOSEPAR)
+    {
+        // regle 2
+        return new Etat1();
+    }
+    else if (s == FIN)
+    {
+        // regle 2
+        return new Etat0();
+    }
+    return false;
+}
+
+Etat *Etat8::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        // regle 3
+        return new Etat0();
+    }
+    else if (s == MULT)
+    {
+        // regle 3
+        return new Etat2();
+    }
+    else if (s == CLOSEPAR)
+    {
+        // regle 3
+        return new Etat1();
+    }
+    else if (s == FIN)
+    {
+        // regle 3
+        return new Etat0();
+    }
+    return false;
+}
+
+Etat *Etat9::transition(Automate &d, Symbole s)
+{
+    if (s == PLUS)
+    {
+        // regle 4
+        return new Etat0();
+    }
+    else if (s == MULT)
+    {
+        // regle 4
+        return new Etat2();
+    }
+    else if (s == CLOSEPAR)
+    {
+        // regle 4
+        return new Etat1();
+    }
+    else if (s == FIN)
+    {
+        // regle 4
+        return new Etat0();
+    }
+    return false;
 }
--- etat.h
+++ etat.h
@@ -1,77 +1,94 @@
 #pragma once
 
 #include "symbole.h"
+#include "automate.h"
 #include <string>
 using namespace std;
 
-typedef struct Automate {} Automate;
+class Etat
+{
 
-class Etat {
-
-   public:
-      virtual ~Etat() { }
-      virtual Etat * transition(Automate & d, Symbole s) = 0;
+public:
+      virtual ~Etat() {}
+      virtual Etat *transition(Automate &d, Symbole s) = 0;
 };
 
-class Etat0 : public Etat{
-    public:
-      Etat0() { }
-      virtual ~Etat0() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat0 : public Etat
+{
+public:
+      Etat0() {}
+      ~Etat0() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat1 : public Etat{
-    public:
-      Etat1() { }
-      virtual ~Etat1() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat1 : public Etat
+{
+public:
+      Etat1() {}
+      ~Etat1() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat2 : public Etat{
-    public:
-      Etat2() { }
-      virtual ~Etat2() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat2 : public Etat
+{
+public:
+      Etat2() {}
+      ~Etat2() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat3 : public Etat{
-    public:
-      Etat3() { }
-      virtual ~Etat3() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat3 : public Etat
+{
+public:
+      Etat3() {}
+      ~Etat3() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat4 : public Etat{
-    public:
-      Etat4() { }
-      virtual ~Etat4() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat4 : public Etat
+{
+public:
+      Etat4() {}
+      ~Etat4() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat5 : public Etat{
-    public:
-      Etat5() { }
-      virtual ~Etat5() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat5 : public Etat
+{
+public:
+      Etat5() {}
+      ~Etat5() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat6 : public Etat{
-    public:
-      Etat6() { }
-      virtual ~Etat6() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat6 : public Etat
+{
+public:
+      Etat6() {}
+      ~Etat6() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat7 : public Etat{
-    public:
-      Etat7() { }
-      virtual ~Etat7() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat7 : public Etat
+{
+public:
+      Etat7() {}
+      ~Etat7() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
 
-class Etat8 : public Etat{
-    public:
-      Etat8() { }
-      virtual ~Etat8() { }
-      virtual Etat * transition(Automate & d, Symbole s);
+class Etat8 : public Etat
+{
+public:
+      Etat8() {}
+      ~Etat8() {}
+      virtual Etat *transition(Automate &d, Symbole s);
+};
+
+class Etat9 : public Etat
+{
+public:
+      Etat9() {}
+      ~Etat9() {}
+      virtual Etat *transition(Automate &d, Symbole s);
 };
