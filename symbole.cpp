6fe8e11524d7ea60702dce4f57ee306ab994c497
Taha Mdarhri <t.mdarhri@gmail.com>
2022-02-15 10:01:39 +0100

    Symbole : entier et expr

--- symbole.cpp
+++ symbole.cpp
@@ -1,12 +1,13 @@
 #include "symbole.h"
 #include <iostream>
 
-void Symbole::Affiche() {
-   cout<<Etiquettes[ident];
+void Symbole::Affiche()
+{
+   cout << Etiquettes[ident];
 }
 
-void Entier::Affiche() {
+void Entier::Affiche()
+{
    Symbole::Affiche();
-   cout<<"("<<valeur<<")";
+   cout << "(" << valeur << ")";
 }
-
